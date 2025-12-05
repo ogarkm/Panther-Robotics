cb15556b8da6f9eb2b56008b6b447826e766e10b
Sanabhi <113478613+SanabhiG@users.noreply.github.com>
2025-12-04 21:49:52 -0600

    Update ThreeDeadWheelLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeDeadWheelLocalizer.java
@@ -21,9 +21,9 @@ import org.firstinspires.ftc.teamcode.messages.ThreeDeadWheelInputsMessage;
 @Config
 public final class ThreeDeadWheelLocalizer implements Localizer {
     public static class Params {
-        public double par0YTicks = 1 / 0.02336; // y position of the first parallel encoder (in tick units)
-        public double par1YTicks = 1 / 0.02336; // y position of the second parallel encoder (in tick units)
-        public double perpXTicks = 1 / 0.02336 ; // x position of the perpendicular encoder (in tick units)
+        public double par0YTicks = 5.8 / 0.02336; // y position of the first parallel encoder (in tick units)
+        public double par1YTicks = 5.8 / 0.02336; // y position of the second parallel encoder (in tick units)
+        public double perpXTicks = 4.9 / 0.02336 ; // x position of the perpendicular encoder (in tick units)
     }
 
     public static Params PARAMS = new Params();
